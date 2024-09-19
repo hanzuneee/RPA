1723929644d09ca3683b4b2a0931f64f88e452bd
[hanzuneee] <[hanzuneee@naver.com]>
2024-09-19 11:36:27 +0900

    3주차 실습

---
+++ 3week/ex3.py
@@ -0,0 +1,14 @@
+num = int(input("1~9 사이의 정수를 입력해주세요."))
+i = 0
+
+for i in range(1, 10):
+    print(f"{num} X {i} = {num*i}")
+
+# i = 0 
+# num = int(input("1~9 사이의 정수를 입력해주세요."))
+# if num > 0 and num < 10: 
+#     for i in range(1, 10):
+#     print(f"{num} X {i} = {num*i}")
+    
+# else:
+#     print("1~9 사이의 정수를 입력해주세요.")
