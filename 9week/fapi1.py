53dfd761f0e3b343f2cd2eaff919ea787e083bfa
[hanzuneee] <[hanzuneee@naver.com]>
2024-11-07 12:17:45 +0900

    9주차 실습파일

--- 9week/fapi1.py
+++ 9week/fapi1.py
@@ -1,37 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile
+from pathlib import Path
+import shutil
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello" : "World"}
-
-@app.get("/item")
-def read_item(item_id: int, name: str = None, age: int = 0):
-    return{"item_id": item_id, "name": name, "age": age}
-
-from fastapi import Form
 @app.post("/plus")
-async def plus_form(num1: int = Form(...), num2 : int = Form(...)):
-    result = num1 + num2    
-    return {"msg": f"{num1} + {num2} = {result}"}
+async def plus_form(num1: int = Form(...), num2: int = Form(...), num3: int = Form(...), num4: int = Form(...)):
+    result1 = num1 + num2
+    result2 = num3 + num4
+    return {"result": f"{num1} + {num2} = {result1} | {num3} + {num4} = {result2}"}
 
-from fastapi import File, UploadFile
-import shutil
-from pathlib import Path
-
-@app.post("/uploadfile")
+@app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     save_path = Path("static/uploads") / file.filename
     save_path.parent.mkdir(parents=True, exist_ok=True)
     
     with save_path.open("wb") as buffer:
-        shutil.copyfile(file.file, buffer)
+        shutil.copyfileobj(file.file, buffer)
+        
+    return {"filename": file.filename, "location": str(save_path)}
 
-    return{"filename": file.filename, "location": str(save_path)}
-
-# if문 위로 항상
-from fastapi.staticfiles import StaticFiles
-app.mount("/", StaticFiles(directory="static", html=True), name="static") 
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == '__main__':
     import uvicorn
