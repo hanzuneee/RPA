853bd626e985b9bcaf4bf89f72279f64643a5a3c
[hanzuneee] <[hanzuneee@naver.com]>
2024-11-21 12:26:43 +0900

    11주차 실습파일

--- 11week/ex4.py
+++ 11week/ex4.py
@@ -1,22 +1,10 @@
-# import qrcode
-
-# name = input("이름을 입력하시오 :")
-# b = input("학번을 입력하시오 :")
-# c = input("전공을 입력하시오 :")
-
-# qr_data = ['a', 'b', 'c']
-# qr_img = qrcode.make(qr_data)
-
-# save__path = 'my_info_data.png'
-# qr_img.save(save__path)
-
 import qrcode 
 
 name = input("이름을 입력하세요: ")
 stdtnum = input("학번을 입력하세요: ")
 major = input("전공을 입력하세요: ")
 
-qr_data = [name, stdtnum, major]
+qr_data = f"{name} {stdtnum} {major}"
 qr_img = qrcode.make(qr_data)
 
 save_path = 'my_info_data.png'
